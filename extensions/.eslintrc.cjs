6ca95aa7dfd9970a10ee7fef861c2de04d7aff33
Matt J Bordenet <bordenet@users.noreply.github.com>
2025-11-06 12:22:59 -0800

    fix: restore missing eslintrc files

--- extensions/.eslintrc.cjs
+++ extensions/.eslintrc.cjs
@@ -1,3 +1,20 @@
-// Forward to root ESLint configuration
-const path = require("path");
-module.exports = require(path.resolve(__dirname, "../.eslintrc.cjs"));
+/* eslint-env node */
+module.exports = {
+  extends: [require.resolve('../.eslintrc.cjs')],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    'no-console': 'off',
+    'no-undef': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
+};
